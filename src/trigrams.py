568bc35d5379d1410532247c89800a769c071d85
yanil3500 <yanil3500@gmail.com>
2017-05-17 15:37:20 -0700

    successfully create trigram from given string

--- src/trigrams.py
+++ src/trigrams.py
@@ -14,23 +14,21 @@ def solution_trigrams(text, number_of_words):
 
 def create_trigrams(text):
     text_list = text.split(' ')
-    print(text_list)
     words_trigrams = {}
-    previous = 0
     for index in range(len(text_list)-1):
-        if index != len(text_list):
-            trigram_key = text_list[previous]+' '+text_list[previous + 1]
-            previous = index
+        trigram_key = text_list[index]+' '+text_list[index + 1]
         if trigram_key not in words_trigrams:
-            words_trigrams[trigram_key] = [text_list[previous]]
+            if index != len(text_list) - 2:
+                words_trigrams[trigram_key] = [text_list[index + 2]]
         else:
-            words_trigrams[trigram_key].append(text_list[index])
-    print("Trigram dictionary: ", words_trigrams)
+            if index == len(text_list)-1:
+                words_trigrams[trigram_key].append(text_list[index])
+            words_trigrams[trigram_key].append(text_list[index + 2])
+    return words_trigrams
 
 
 
-
-create_trigrams('I wish I may I wish I might')
+print(create_trigrams('I wish I may I wish I might'))
 if __name__ == '__main__':
     solution_trigrams(sys.argv[1], sys.argv[2])
 
