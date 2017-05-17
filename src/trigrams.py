8cc30e358ac08dd6eae1e21b74bdaf9ea6e14a55
yanil3500 <yanil3500@gmail.com>
2017-05-17 14:08:09 -0700

    creates test for create_trigrams

--- src/trigrams.py
+++ src/trigrams.py
@@ -12,6 +12,11 @@ def solution_trigrams(text, number_of_words):
     print('Text: ', text)
 
 
+def create_trigrams(text):
+    print('test string')
+    return None
+
+
 if __name__ == '__main__':
     solution_trigrams(sys.argv[1], sys.argv[2])
 
