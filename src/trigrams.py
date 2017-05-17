c564f0f7a8a3cd5b6ea83e19fd1c1da1721065dd
yanil3500 <yanil3500@gmail.com>
2017-05-17 16:21:59 -0700

    attempts to remove newlines characters from text

--- src/trigrams.py
+++ src/trigrams.py
@@ -10,14 +10,14 @@ def solution_trigrams(text, number_of_words):
     """
     print('Number of words: ', number_of_words)
     print('Text: ', text)
+    with open(text) as infile:
+        data = infile.read()
+        create_trigrams(data)
 
 
 def create_trigrams(text):
-    from string import punctuation
-    exclude_punctuation = set(punctuation)
-    text_list = ''.join(punc for punc in text if punc not in exclude_punctuation)
-    text_list = text_list.split(' ')
-    print(text_list)
+    text_list = remove_punctuation(text).split(' ')
+    print('Inside of create_trigrams: ', text_list)
     words_trigrams = {}
     for index in range(len(text_list)-1):
         trigram_key = text_list[index]+' '+text_list[index + 1]
@@ -31,8 +31,16 @@ def create_trigrams(text):
     return words_trigrams
 
 
+def remove_punctuation(text):
+    """
+    removes punctuation and newlines
+    """
+    from string import punctuation
+    exclude_punctuation = set(punctuation)
+    text_list = ''.join(punc for punc in text if punc not in exclude_punctuation)
+    return text_list
 
-print(create_trigrams('I wish, I may, I wish ,I might.'))
+
 if __name__ == '__main__':
     solution_trigrams(sys.argv[1], sys.argv[2])
 
